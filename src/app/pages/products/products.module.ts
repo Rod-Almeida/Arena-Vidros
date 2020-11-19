b83ac8cf5528c07f8fbf18ce0c4fb382ae8a8a46
Rodrigo Almeida <rod.almeida01@gmail.com>
2020-11-19 13:05:48 -0300

    Products Table

--- src/app/pages/products/products.module.ts
+++ src/app/pages/products/products.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsComponent } from './products.component';
 import { MatCardModule } from '@angular/material/card';
-
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [ProductsComponent],
   imports: [
     CommonModule,
     MatCardModule,
+    MatTableModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
   ]
 })
 export class ProductsModule { }
